685c4067048f47cd6539dc72b499faa38a5f3954
NK-Works <132702983+NK-Works@users.noreply.github.com>
2023-10-01 18:10:21 +0530

    Add files via upload

---
+++ Task7.2D/src/routes/postpage/PostPage.jsx
@@ -0,0 +1,16 @@
+/* This code is made by Anneshu Nag, Student ID- 2210994760  */
+/*                    Dated- 21/09/2023                      */
+
+import NewPost from './NewPost';  // Importing the NewPost Component
+import ProblemAsk from './ProblemAsk';   // Importing the ProblemAsk Component
+
+function PostPage() {
+  return (
+    <div>
+      <NewPost />
+      <ProblemAsk />
+    </div>
+  );
+}
+
+export default PostPage;
